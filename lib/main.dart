91b576233f114fc6e21c728b617efa92bd5fc1e3
nooh <nooh.saeed@gmail.com>
2024-09-23 09:21:11 +0300

    init setting and create folder utilis

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:al_najah_store/app.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main()async{
+
+
+  runApp( const App());  
+}
---
+++ lib/utilis/constants/text_strings.dart
@@ -0,0 +1,61 @@
+class NTexts {
+// Global Texts
+//OnBoarding Texts
+  static const String OnBoardingTitle1 = "Choose your product";
+  static const String OnBoardingTitle2 = "Select Payment Method";
+  static const String OnBoardingTitle3 = "Deliver at your door step";
+
+  static const String OnBoardingSubTitle1 =
+      "Welcome to a World of Limitless Choice -Your Product Awaits";
+  static const String OnBoardingSubTitle2 =
+      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority! ";
+  static const String OnBoardingSubTitle3 =
+      "Form Our Doorstep to Yours - Swift, Secure, and Contacless Delivery";
+//Home
+  static const String homeAppbarTitle = "Good day for Shoping";
+  static const String homrAppbarSubTitle = "Nooh Saeed";
+
+
+
+
+
+
+//Login
+  static const String loginTitle = "Welcome back";
+  static const String loginSubTitle = "Discover Limitless Choice and Unmatched Convenience.";
+    static const String siginTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forget password";
+  static const String forgetPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password reset like.";
+  static const String changeYourPasswordTitle = "Password Reset Email Send";
+  static const String  changeYourPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password reset like.";
+
+
+
+// Authentication Form Test
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+    static const String email = "E-Mail";
+  static const String password = "Password";
+    static const String newPassword = "New Password";
+        static const String phoneNumber = "Phone Number";
+    static const String userName = "UserName";
+    static const String rememberMe = "Rememer Me ";
+    static const String forgetPassword = "Forget Password?";
+    static const String signIn = "Sign In";
+    static const String createAccount = "Create Account";
+        static const String orSignInWith = "or sign in with";
+        static const String orSignUpWith = "or sign up with";
+                static const String iAgreeTO = "I Agree To";
+                        static const String privacyPolicy = "Privacy Policy";
+        static const String termsOfUse = "Terms of use";
+        static const String verificationCode = "Verification Code";
+        static const String resendEmail = "Resend Email";
+        static const String resendEmailIn = "Resend email in";
+
+
+
+
+
+
+
+}
